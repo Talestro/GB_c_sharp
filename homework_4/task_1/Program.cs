a950df0616be7e6c75db7f223a3aac0875a2c709
Talestro <SemenovEM@yandex.ru>
2022-08-09 19:25:19 +0300

    task 1 of homework 4 fixed

--- homework_4/task_1/Program.cs
+++ homework_4/task_1/Program.cs
@@ -3,10 +3,21 @@
 // 2, 4 -> 16
 
 Console.WriteLine("Введите число");
-double number = double.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите степень для первого числа");
-double degree = double.Parse(Console.ReadLine());
+int degree = int.Parse(Console.ReadLine());
 
-double result = Math.Pow(number, degree);       //полагаю, что в условии задания ошибка, т.к. возведение двух числе не требует цикла ибо это всего один шаг, исходя из этого реализовано без цикла
+int result = 1;
 
+if (degree == 1)
+{
+    result = number;
+}
+else
+{
+    for (int i = 0; i < degree; i++)
+    {
+        result *= number;
+    }
+}
 Console.WriteLine("Число " + number + " в степени " + degree + " равно: " + result);
