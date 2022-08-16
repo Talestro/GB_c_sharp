36484894f35660f5b077357001c308c2fbe5f75b
Talestro <SemenovEM@yandex.ru>
2022-08-16 18:59:07 +0300

    added homework_6

---
+++ homework_6/task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Enter how much numbers you whant enter:");
+int m = int.Parse(Console.ReadLine());
+
+int[] userNumbers = new int [m];
+
+getNumbersFromUser(userNumbers);
+Console.WriteLine("You entered " + getResult(userNumbers) + " numbers thet > 0");
+
+
+int getResult (int [] array) //подсчёт моджно было делать сразу при запросе и не хранить ввод в отдельном массиве. По сути - массив тут избыточен, и если введённые данные хранить не треубуется - можно сократить решение
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+void getNumbersFromUser (int [] array)
+{
+    Console.Write("Enter any numbers. Enter - 'q' key to stop entering numbers\n");
+    string input;
+    for(int i = 0; i < array.Length; i++)
+    {
+        input = Console.ReadLine();
+        if(input[0] == 'q')
+        {
+            return;
+        }
+        else
+        {
+            array[i] = int.Parse(input);
+        }
+    }
+}
---
+++ homework_6/task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double [,] userCoordinates = new double [2,2];
+
+getInputFromUser(userCoordinates);
+getIntersectionCoordinates(userCoordinates);
+
+
+
+void getIntersectionCoordinates (double [,] array)
+{
+    double intersectionX = (array[0,0] - array[0,1]) / (array[1,0] - array[1,1]) * (-1);   //(b1 - b2) / (k1 - k2) * -1
+    double intersectionY = array[1,1] * intersectionX + array[0,1];                        //k2 * x + b2
+    Console.WriteLine("Intersection point coordinates is: (" + intersectionX + "; " + intersectionY + ")");
+}
+
+void getInputFromUser(double [,] array) //по сути массив тут не нужен и избыточен, т.к. проще было бы запросить 4 координаты у пользоателя без циклов и массива. Прошу прощения, если это сложно проверять - хотел потренировать двумерные массивы
+{
+    int j;
+    char coordinate = 'b';
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine("Enter " + coordinate + (j+1));
+            array[i,j] = double.Parse(Console.ReadLine());
+        }
+        coordinate = 'k';
+        j = 0;
+    }
+}
