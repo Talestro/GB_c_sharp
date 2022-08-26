0d6634b51d2d3e2e9cbcc38f89d3aa1dfb209fe9
Talestro <SemenovEM@yandex.ru>
2022-08-26 14:59:12 +0300

    added homework_9

---
+++ homework_9/task_1/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.WriteLine("Enter M:");
+int startM = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter N:");
+int endN = int.Parse(Console.ReadLine());
+Console.WriteLine("Use recursion? (y/n)");
+ConsoleKeyInfo userAnswer = Console.ReadKey();
+Console.WriteLine();
+
+if (userAnswer.Key == ConsoleKey.Y && startM < endN)
+{
+    Console.Write("\"\"");
+    getNumbersRecursion(startM, endN);
+    Console.Write("\"\"");
+}
+else if (userAnswer.Key == ConsoleKey.N && startM < endN)
+{
+    Console.Write("\"\"");
+    getNumbers(startM, endN);
+    Console.Write("\"\"");
+}
+else
+{
+    Console.WriteLine("Incorrect inputs. The program will be terminated. Try again");
+    return;
+}
+
+
+void getNumbers (int start, int end) //вариант без рекурсии, на мой взгляд, оптимальнее, т.к. короче и ест меньше ресурсов при выполнении
+{
+    do
+    {
+        if (start != end)
+        {
+            Console.Write(start + ", ");
+        }
+        else
+        {
+            Console.Write(start);
+        }
+        start++;
+    } while (start <= end);    
+}
+
+
+void getNumbersRecursion (int start, int end)
+{
+    if (start > end)
+    {
+        return;
+    }
+    if (start != end)
+    {
+        Console.Write(start + ", ");
+    }
+    else
+    {
+        Console.Write(start);
+    }
+    start++;
+    getNumbersRecursion(start, end);
+}
---
+++ homework_9/task_2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Enter M:");
+int startM = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter N:");
+int endN = int.Parse(Console.ReadLine());
+Console.WriteLine("Use recursion? (y/n)");
+ConsoleKeyInfo userAnswer = Console.ReadKey();
+Console.WriteLine();
+int sum = 0;
+
+if (userAnswer.Key == ConsoleKey.Y && startM < endN)
+{
+    getNumbersRecursion(startM, endN, sum);
+}
+else if (userAnswer.Key == ConsoleKey.N && startM < endN)
+{
+    getNumbers(startM, endN, sum);
+}
+else
+{
+    Console.WriteLine("Incorrect inputs. The program will be terminated. Try again");
+    return;
+}
+
+
+void getNumbers (int start, int end, int sum) //вариант без рекурсии, на мой взгляд, оптимальнее, т.к. короче и ест меньше ресурсов при выполнении
+{
+    do
+    {
+        sum += start;
+        start++;
+    } while (start <= end);
+    Console.Write($"Sum of numbers from M to N is: {sum}");
+}
+
+
+void getNumbersRecursion (int start, int end, int sum)
+{
+    if (start > end)
+    {
+        Console.Write($"Sum of numbers from M to N is: {sum}");
+        return;
+    }
+    sum += start;
+    start++;
+    getNumbersRecursion(start, end, sum);
+}
